1ad82642b19d61bf95fdfac62b8805ae4f604b92
LAURA CECILIA MENDOZA MORALES <lcmm97.lcmm@gmail.com>
2024-04-17 11:44:08 -0500

    Creación componentes y enrutado

--- frontend/e-smarttachito/src/app/app.routes.ts
+++ frontend/e-smarttachito/src/app/app.routes.ts
@@ -1,3 +1,68 @@
 import { Routes } from '@angular/router';
+import { LandingComponent } from '../landing/landing.component';
+import { LoginComponent } from '../login/login.component';
+import { CarritoComponent } from '../carrito/carrito.component';
+import { CatalogoComponent } from '../catalogo/catalogo.component';
+import { DetalleCompraComponent } from '../detalle-compra/detalle-compra.component';
+import { ListaComprasComponent } from '../lista-compras/lista-compras.component';
+import { PagoComponent } from '../pago/pago.component';
+import { ProductoComponent } from '../producto/producto.component';
+import { RegistroComponent } from '../registro/registro.component';
+import { UsuarioComponent } from '../usuario/usuario.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        component: LandingComponent,
+        title: 'Smart Tachito',
+        children: [
+            {
+                path:'login',
+                component: LoginComponent,
+                title: 'login'
+            },
+            {
+                path:'carrito',
+                component: CarritoComponent,
+                title: 'carrito'
+            },
+            {
+                path:'catalogo',
+                component: CatalogoComponent,
+                title: 'catalogo'
+            },
+            {
+                path:'detalle-compra',
+                component: DetalleCompraComponent,
+                title: 'detalle-compra'
+            },
+            {
+                path:'pago',
+                component: PagoComponent,
+                title: 'pago'
+            },
+            {
+                path:'producto',
+                component: ProductoComponent,
+                title: 'producto'
+            },
+            {
+                path:'registro',
+                component: RegistroComponent,
+                title: 'registro'
+            },
+            {
+                path:'usuario',
+                component: UsuarioComponent,
+                title: 'usuario',
+                children: [
+                    {
+                        path:'lista-compras',
+                        component: ListaComprasComponent,
+                        title: 'lista-compras'
+                    }
+                ]
+            }
+        ]
+    } 
+];
---
+++ frontend/e-smarttachito/src/detalle-compra/detalle-compra.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detalle-compra',
+  standalone: true,
+  imports: [],
+  templateUrl: './detalle-compra.component.html',
+  styleUrl: './detalle-compra.component.css'
+})
+export class DetalleCompraComponent {
+
+}
