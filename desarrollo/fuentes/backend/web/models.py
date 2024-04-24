7826b5f6210d948ea8006af087a11305c1e6f602
Jorge Ktch <jorgektch@gmail.com>
2024-04-24 15:38:03 -0500

    Crear modelo Categoria

--- desarrollo/fuentes/backend/web/models.py
+++ desarrollo/fuentes/backend/web/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Categoria(models.Model):
+    nombre = models.CharField("Nombre", max_length=100)
+    descripcion = models.CharField("Descripción", max_length=500)
+    imagem = models.ImageField("Imagen reportada", upload_to="frames/")
+    def __str__(self):
+        return self.name
+    class Meta:
+        verbose_name = "Categoria"
+        verbose_name_plural = "Categorias"
