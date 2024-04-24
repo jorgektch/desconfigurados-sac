14465c5a6d51748a57fc90825b76bb1e7c02242a
Jorge Ktch <jorgektch@gmail.com>
2024-04-24 17:50:43 -0500

    Actualizar admin

--- desarrollo/fuentes/backend/web/admin.py
+++ desarrollo/fuentes/backend/web/admin.py
@@ -6,4 +6,9 @@ class CategoriaAdmin(admin.ModelAdmin):
     list_display = [field.name for field in Categoria._meta.fields]
     ordering = ('nombre',)
 
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Producto._meta.fields]
+    ordering = ('nombre',)
+
 admin.site.register(Categoria, CategoriaAdmin)
+admin.site.register(Producto, ProductoAdmin)
