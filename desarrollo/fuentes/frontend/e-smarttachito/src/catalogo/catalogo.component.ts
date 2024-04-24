6e9daeec6233e21b793dbc40cbe43a42cf004ad9
anycodef <anycodefast1@gmail.com>
2024-04-24 17:40:47 -0500

    aniadir[frontend]: logo, brand color, nuevo formato para mostrar los productos

--- desarrollo/fuentes/frontend/e-smarttachito/src/catalogo/catalogo.component.ts
+++ desarrollo/fuentes/frontend/e-smarttachito/src/catalogo/catalogo.component.ts
@@ -25,12 +25,14 @@ export class CatalogoComponent implements OnInit {
     // metodo para actualizar lista de productos
     
     // Simulación de la obtención de productos
-    const producto1 = new Producto(1, 'Producto 1', 'Descripción del Producto 1', 'Corta Descripción 1', 'Col1', 'Col2', 100, ['imagen1.jpg'], 1);
-    const producto2 = new Producto(2, 'Producto 2', 'Descripción del Producto 2', 'Corta Descripción 2', 'Col1', 'Col2', 200, ['imagen2.jpg'], 2);
-    
     // Agregar los productos a la lista
-    this.productos.push(producto1, producto2);
-    
+    this.productos.push(new Producto(1, 'Producto 1', 'Descripción del Producto 1', 'Corta Descripción 1', 'Col1', 'Col2', 100, ['imagen1.jpg'], 1),
+                        new Producto(2, 'Producto 2', 'Descripción del Producto 2', 'Corta Descripción 2', 'Col1', 'Col2', 200, ['imagen2.jpg'], 2),
+                        new Producto(3, 'Producto 3', 'Descripción del Producto 3', 'Corta Descripción 3', 'Col1', 'Col2', 150, ['imagen3.jpg'], 1),
+                        new Producto(4, 'Producto 4', 'Descripción del Producto 4', 'Corta Descripción 4', 'Col1', 'Col2', 180, ['imagen4.jpg'], 2),
+                        new Producto(5, 'Producto 5', 'Descripción del Producto 5', 'Corta Descripción 5', 'Col1', 'Col2', 120, ['imagen5.jpg'], 1),
+                        new Producto(6, 'Producto 6', 'Descripción del Producto 6', 'Corta Descripción 6', 'Col1', 'Col2', 250, ['imagen6.jpg'], 2),
+                        new Producto(7, 'Producto 7', 'Descripción del Producto 7', 'Corta Descripción 7', 'Col1', 'Col2', 300, ['imagen7.jpg'], 1))
   }
 
   agregarAlCarrito() {
