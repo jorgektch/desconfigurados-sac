b648458e40d27b7841d5f780da19199ec8463de6
anycodef <anycodefast1@gmail.com>
2024-04-28 01:26:00 -0500

    aniadir[frontend-carrito.component.ts]: funcionalidades recalcularTotal, obtenerElTotal y aniadimos rxjs para manerjar con observables.

--- desarrollo/smarttachito/fuentes/frontend/e-smarttachito/src/carrito/carrito.component.ts
+++ desarrollo/smarttachito/fuentes/frontend/e-smarttachito/src/carrito/carrito.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Producto } from '../producto/producto.class';
 import { NgIf } from '@angular/common';
+import { BehaviorSubject, Observable, from, map } from 'rxjs';
 
 @Component({
     selector: 'carrito',
@@ -9,32 +10,49 @@ import { NgIf } from '@angular/common';
     styleUrl: './carrito.component.css',
     imports: [NgIf]
 })
-export class CarritoComponent {
+export class CarritoComponent implements OnInit {
   productosEnCarrito: Producto[] = [];
   seVisualiza: Boolean = false;
 
+  totalCarrito$ = new BehaviorSubject<number>(0) // Observable para la cantidad total
+
+  constructor () {}
+
+  ngOnInit(): void {
+    from(this.productosEnCarrito).subscribe(() => this.recalcularTotal());
+  }
+
   agregarProductoAlCarrito(idProducto: Producto) {
     this.productosEnCarrito.push(idProducto);
+    this.recalcularTotal();
   }
 
   eliminarProductoDelCarrito(idProducto: Producto) {
     const indexProductoEliminar = this.productosEnCarrito.indexOf(idProducto);
     if(indexProductoEliminar != -1) {
       this.productosEnCarrito.splice(indexProductoEliminar, 1);
+      this.recalcularTotal()
     }
   }
 
   vaciarElCarrito() {
     this.productosEnCarrito = [];
+    this.recalcularTotal();
   }
 
   obtenerCantidadProductos(): number {
     return this.productosEnCarrito.length;
   }
 
-  obtenerElTotal(): number {
+  obtenerElTotal(): Observable<number> {
     // lógica para implementar el cálculo total a pagar
-    return 0;
+    const total = this.productosEnCarrito.reduce((acc, producto) => acc + producto.precio, 0);
+    this.totalCarrito$.next(total);
+    return this.totalCarrito$.asObservable();
+  }
+
+  private recalcularTotal() {
+    this.obtenerElTotal();
   }
 
   toggleVisualizacionCarrito() {
