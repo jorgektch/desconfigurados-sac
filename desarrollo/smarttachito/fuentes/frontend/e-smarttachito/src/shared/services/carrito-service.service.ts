55ce8e90f772b7b86bd2c19da913a8d1a87f5dbd
anycodef <anycodefast1@gmail.com>
2024-04-29 22:21:19 -0500

    Añadir en el servicio carrito lista de productos y agregar producto, además un observable con el que pueden de forma reactiva ver los productos añadidos

--- desarrollo/smarttachito/fuentes/frontend/e-smarttachito/src/shared/services/carrito-service.service.ts
+++ desarrollo/smarttachito/fuentes/frontend/e-smarttachito/src/shared/services/carrito-service.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { Productos } from '../interfaces/interface';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CarritoServiceService {
 
-  constructor() { }
+  // lista de productos del servicio
+  private productosCarrito: Productos[] = [];
+
+  // lista de productos que se emitiran a todos los componentes que se suscriptores
+  private _listaProductos: BehaviorSubject<Productos[]>;
+
+  constructor() {
+    // inicializamos con una lista vacia
+    this._listaProductos = new BehaviorSubject<Productos[]>([]);
+   }
+
+   // funcionalidad para retornar como observable a nuestra lista de productos
+   get _listaProductosObservable() {
+    return this._listaProductos.asObservable();
+   }
+   
+   // implementamos la funcionalidad añadir producto y emitimos la nueva lista
+   agregarProducto(producto: Productos) {
+    this.productosCarrito.push(producto);
+    this._listaProductos.next(this.productosCarrito);
+   }
+
 }
