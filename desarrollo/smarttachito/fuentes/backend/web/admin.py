decd7dc14350225519edec7b690e15381eebb438
Jorge Ktch <jorgektch@gmail.com>
2024-05-02 07:56:08 -0500

    Separar el proyecto django en aplicaciones

--- desarrollo/smarttachito/fuentes/backend/web/admin.py
+++ desarrollo/smarttachito/fuentes/backend/web/admin.py
@@ -1,37 +1,9 @@
 from django.contrib import admin
 from .models import *
 
-class CategoriaAdmin(admin.ModelAdmin):
-    list_display = [field.name for field in Categoria._meta.fields]
-    ordering = ('nombre',)
-
-class ProductoAdmin(admin.ModelAdmin):
-    list_display = [field.name for field in Producto._meta.fields]
-    ordering = ('nombre',)
-
 class TipoDocumentoAdmin(admin.ModelAdmin):
     list_display = [field.name for field in TipoDocumento._meta.fields]
     ordering = ('nombre',)
-
-class UsuarioAdmin(admin.ModelAdmin):
-    list_display = [field.name for field in Usuario._meta.fields]
-    ordering = ('username',)
-
-class ClienteAdmin(admin.ModelAdmin):
-    list_display = [field.name for field in Cliente._meta.fields]
-    ordering = ('usuario',)
-
-class EmpleadoAdmin(admin.ModelAdmin):
-    list_display = [field.name for field in Empleado._meta.fields]
-    ordering = ('usuario',)
-
-class ResiduoAdmin(admin.ModelAdmin):
-    list_display = [field.name for field in Residuo._meta.fields]
-    ordering = ('nombre',)
-
-class ContenedorAdmin(admin.ModelAdmin):
-    list_display = [field.name for field in Contenedor._meta.fields]
-    ordering = ('producto', 'residuo',)
 
 class PaisAdmin(admin.ModelAdmin):
     list_display = [field.name for field in Pais._meta.fields]
@@ -41,48 +13,16 @@ class CiudadAdmin(admin.ModelAdmin):
     list_display = [field.name for field in Ciudad._meta.fields]
     ordering = ('nombre',)
 
-class UbicacionAdmin(admin.ModelAdmin):
-    list_display = [field.name for field in Ubicacion._meta.fields]
-    ordering = ('ciudad', 'direccion',)
-
 class EstadoEntregaAdmin(admin.ModelAdmin):
     list_display = [field.name for field in EstadoEntrega._meta.fields]
     ordering = ('nombre',)
-
-class EntregaAdmin(admin.ModelAdmin):
-    list_display = [field.name for field in Entrega._meta.fields]
-    ordering = ('ubicacion', 'empleado', 'fechahora_entrega',)
-
-class PagoAdmin(admin.ModelAdmin):
-    list_display = [field.name for field in Pago._meta.fields]
-    ordering = ('fechahora_pago', 'monto',)
 
 class EstadoOrdenAdmin(admin.ModelAdmin):
     list_display = [field.name for field in EstadoOrden._meta.fields]
     ordering = ('nombre',)
 
-class OrdenAdmin(admin.ModelAdmin):
-    list_display = [field.name for field in Orden._meta.fields]
-    ordering = ('cliente', 'fechahora_orden',)
-
-class DetalleOrdenAdmin(admin.ModelAdmin):
-    list_display = [field.name for field in DetalleOrden._meta.fields]
-    ordering = ('orden', 'producto',)
-
-admin.site.register(Categoria, CategoriaAdmin)
-admin.site.register(Producto, ProductoAdmin)
 admin.site.register(TipoDocumento, TipoDocumentoAdmin)
-admin.site.register(Usuario, UsuarioAdmin)
-admin.site.register(Cliente, ClienteAdmin)
-admin.site.register(Empleado, EmpleadoAdmin)
-admin.site.register(Residuo, ResiduoAdmin)
-admin.site.register(Contenedor, ContenedorAdmin)
 admin.site.register(Pais, PaisAdmin)
 admin.site.register(Ciudad, CiudadAdmin)
-admin.site.register(Ubicacion, UbicacionAdmin)
 admin.site.register(EstadoEntrega, EstadoEntregaAdmin)
-admin.site.register(Entrega, EntregaAdmin)
-admin.site.register(Pago, PagoAdmin)
 admin.site.register(EstadoOrden, EstadoOrdenAdmin)
-admin.site.register(Orden, OrdenAdmin)
-admin.site.register(DetalleOrden, DetalleOrdenAdmin)
--- desarrollo/smarttachito/fuentes/backend/web/models.py
+++ desarrollo/smarttachito/fuentes/backend/web/models.py
@@ -2,43 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.contrib.auth.models import Group
 from django.contrib.auth.hashers import make_password
-
-class Categoria(models.Model):
-    nombre = models.CharField("Nombre", max_length = 100)
-    descripcion = models.CharField("Descripción", max_length = 500)
-    imagen = models.ImageField("Imagen", upload_to = "imagen/")
-    def __str__(self):
-        return self.nombre
-    class Meta:
-        verbose_name = "Categoria de productos"
-        verbose_name_plural = "Categorias de productos"
-
-class Producto(models.Model):
-    nombre = models.CharField("Nombre", max_length = 100)
-    descripcion_breve = models.CharField("Descripción breve", max_length = 100)
-    descripcion_extendida = models.CharField("Descripción extendida", max_length = 500)
-    precio = models.DecimalField("Precio", max_digits=10 , decimal_places = 2)
-    imagen_principal = models.ImageField("Imagen principal", upload_to = "imagen/")
-    imagen_secundaria_1 = models.ImageField("Imagen secundaria 1", upload_to = "imagen/", blank = True, null = True)
-    imagen_secundaria_2 = models.ImageField("Imagen secundaria 2", upload_to = "imagen/", blank = True, null = True)
-    imagen_secundaria_3 = models.ImageField("Imagen secundaria 3", upload_to = "imagen/", blank = True, null = True)
-    imagen_360 = models.ImageField("Imagen 360", upload_to = "imagen/", blank = True, null = True)
-    categoria = models.ForeignKey(Categoria, on_delete = models.CASCADE, verbose_name = "Categoría")
-    stock = models.IntegerField("Stock", blank = True, null = True)
-    def __str__(self):
-        return self.nombre
-    class Meta:
-        verbose_name = "Producto"
-        verbose_name_plural = "Productos"
-
-class TipoDocumento(models.Model):
-    nombre = models.CharField("Nombre", max_length = 100)
-    descripcion = models.CharField("Descripción", max_length = 500)
-    def __str__(self):
-        return self.nombre
-    class Meta:
-        verbose_name = "Tipo de documento"
-        verbose_name_plural = "Tipos de documento"
+from configuracion.models import *
 
 class UsuarioManager(BaseUserManager):
     def create_user(self, nombres, username, email, password = None):
@@ -76,9 +40,12 @@ class Usuario(AbstractBaseUser):
     tipo_documento = models.ForeignKey(TipoDocumento, on_delete = models.CASCADE, verbose_name = "Tipo de documento", null = True, blank = True)
     numero_documento = models.CharField("Número de documento", max_length=100, null=True, blank=True)
     celular = models.CharField("Celular", max_length = 20, blank = True, null = True)
+    #fecha_registro = models.DateTimeField(auto_now_add=True)
     
-    usuario_activo = models.BooleanField(default = True)
-    usuario_administrador = models.BooleanField(default = False)
+    usuario_administrador = models.BooleanField(default=False)
+    usuario_activo = models.BooleanField(default=True)
+    fecha_registro = models.DateTimeField(auto_now_add=True)
+
     objects = UsuarioManager()
 
     USERNAME_FIELD = "username"
@@ -96,6 +63,14 @@ class Usuario(AbstractBaseUser):
     @property
     def is_staff(self):
         return self.usuario_administrador
+    
+    @property
+    def is_active(self):
+        return self.usuario_activo
+
+    @property
+    def date_joined(self):
+        return self.usuario_administrador
 
     def save(self, *args, **kwargs): # Hasheo de contraseñas
         if self.password and not self.password.startswith(('pbkdf2_sha256$', 'bcrypt$', 'argon2')):
@@ -105,15 +80,6 @@ class Usuario(AbstractBaseUser):
     class Meta:
         verbose_name = "Usuario"
         verbose_name_plural = "Usuarios"
-
-class Cliente(models.Model):
-    usuario = models.OneToOneField(Usuario, on_delete = models.CASCADE, verbose_name = "Usuario asociado")
-    fecha_registro = models.DateField("Fecha de registro", auto_now_add = True)
-    def __str__(self):
-        return self.usuario.username
-    class Meta:
-        verbose_name = "Cliente"
-        verbose_name_plural = "Clientes"
 
 class Empleado(models.Model):
     usuario = models.OneToOneField(Usuario, on_delete = models.CASCADE, verbose_name = "Usuario asociado")
@@ -125,117 +91,3 @@ class Empleado(models.Model):
     class Meta:
         verbose_name = "Empleado"
         verbose_name_plural = "Empleados"
-
-class Residuo(models.Model):
-    nombre = models.CharField("Nombre", max_length = 100)
-    descripcion = models.CharField("Descripción", max_length = 500)
-    imagen = models.ImageField("Imagen", upload_to = "imagen/")
-    def __str__(self):
-        return self.nombre
-    class Meta:
-        verbose_name = "Residuo"
-        verbose_name_plural = "Residuos"
-
-class Contenedor(models.Model):
-    producto = models.ForeignKey(Producto, on_delete = models.CASCADE, verbose_name = "Producto")
-    residuo = models.ForeignKey(Residuo, on_delete = models.CASCADE, verbose_name = "Tipo de residuo que puede contener")
-    def __str__(self):
-        return f"{self.producto} {self.residuo}"
-    class Meta:
-        verbose_name = "Contenedor"
-        verbose_name_plural = "Contenedores"
-
-class Pais(models.Model):
-    nombre = models.CharField("Nombre", max_length = 100)
-    def __str__(self):
-        return self.nombre
-    class Meta:
-        verbose_name = "Pais"
-        verbose_name_plural = "Paises"
-
-class Ciudad(models.Model):
-    pais = models.ForeignKey(Pais, on_delete = models.CASCADE, verbose_name = "País")
-    nombre = models.CharField("Nombre", max_length = 100)
-    def __str__(self):
-        return self.nombre
-    class Meta:
-        verbose_name = "Ciudad"
-        verbose_name_plural = "Ciudades"
-
-class Ubicacion(models.Model):
-    ciudad = models.ForeignKey(Ciudad, on_delete = models.CASCADE, verbose_name = "Ciudad")
-    direccion = models.CharField("Dirección", max_length = 1000)
-    referencia = models.CharField("Referencia", max_length = 500)
-    codigo_postal = models.CharField("Código postal", max_length = 20)
-    latitud = models.DecimalField("Latitud", max_digits = 12, decimal_places = 8)
-    longitud = models.DecimalField("Longitud", max_digits = 12, decimal_places = 8)
-    def __str__(self):
-        return f"{self.ciudad} {self.direccion}"
-    class Meta:
-        verbose_name = "Ubicacion"
-        verbose_name_plural = "Ubicaciones"
-
-class EstadoEntrega(models.Model):
-    nombre = models.CharField("Nombre", max_length = 100)
-    descripcion = models.CharField("Descripción", max_length = 500)
-    def __str__(self):
-        return f"{self.nombre}"
-    class Meta:
-        verbose_name = "Estado de la entrega"
-        verbose_name_plural = "Estados de la entrega"
-
-class Entrega(models.Model):
-    ubicacion = models.ForeignKey(Ubicacion, on_delete = models.CASCADE, verbose_name = "Ubicación de entrega")
-    empleado = models.ForeignKey(Empleado, on_delete = models.CASCADE,  blank = True, null = True, verbose_name = "Empleado asignado")
-    fechahora_entrega = models.DateTimeField("Fecha y hora de entrega", auto_now_add = True)
-    detalles_entrega = models.CharField("Anotaciones del encargado de la entrega", max_length = 1000, blank = True, null = True)
-    estado_entrega = models.ForeignKey(EstadoEntrega, on_delete = models.CASCADE, verbose_name = "Estado de la entrega")
-    def __str__(self):
-        return f"{self.ubicacion} {self.empleado} {self.fechahora_entrega}"
-        #return str(self.fecha_entrega)
-    class Meta:
-        verbose_name = "Entrega"
-        verbose_name_plural = "Entregas"
-
-class Pago(models.Model):
-    monto = models.DecimalField("Monto total", max_digits = 16 , decimal_places = 2)
-    fechahora_pago = models.DateTimeField("Fecha y hora de pago", auto_now_add = True)
-    def __str__(self):
-        return f"{self.fecha_pago} {self.monto}"
-        #return str(self.fecha_pago)
-    class Meta:
-        verbose_name = "Pago"
-        verbose_name_plural = "Pagos"
-
-class EstadoOrden(models.Model):
-    nombre = models.CharField("Nombre", max_length = 100)
-    descripcion = models.CharField("Descripción", max_length = 500)
-    def __str__(self):
-        return f"{self.nombre}"
-    class Meta:
-        verbose_name = "Estado de la orden"
-        verbose_name_plural = "Estados de la orden"
-
-class Orden(models.Model):
-    cliente = models.ForeignKey(Cliente, on_delete = models.CASCADE, verbose_name = "Cliente")
-    fechahora_orden = models.DateTimeField("Fecha y hora de registro del orden", auto_now_add = True)
-    estado_orden = models.ForeignKey(EstadoOrden, on_delete = models.CASCADE, verbose_name = "Estado de la orden")
-    entrega = models.ForeignKey(Entrega, on_delete = models.CASCADE, blank = True, null = True, verbose_name = "Entrega")
-    pago = models.ForeignKey(Pago, on_delete = models.CASCADE, blank = True, null = True, verbose_name = "Pago")
-    def __str__(self):
-        return f"{self.cliente} {self.fechahora_orden}"
-        #return str(self.fecha_pedido)
-    class Meta:
-        verbose_name = "Orden"
-        verbose_name_plural = "Órdenes"
-
-class DetalleOrden(models.Model):
-    orden = models.ForeignKey(Orden, on_delete = models.CASCADE, verbose_name = "Orden")
-    producto = models.ForeignKey(Producto, on_delete = models.CASCADE, verbose_name = "Producto")
-    cantidad = models.IntegerField("Cantidad")
-    def __str__(self):
-        return f"{self.orden} {self.producto}"
-        #return self.orden
-    class Meta:
-        verbose_name = "Detalle orden"
-        verbose_name_plural = "Detalles orden"
