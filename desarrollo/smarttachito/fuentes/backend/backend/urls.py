37f0c3b25f856f8dbed3e5b549cd9495953c0a76
Jorge Ktch <jorgektch@gmail.com>
2024-05-02 07:57:45 -0500

    Personalizar títulos del panel de administración

--- desarrollo/smarttachito/fuentes/backend/backend/urls.py
+++ desarrollo/smarttachito/fuentes/backend/backend/urls.py
@@ -19,6 +19,10 @@ from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
 
+admin.site.site_header = 'Ecommerce Smart Tachito'
+admin.site.site_title = "Ecommerce Smart Tachito"
+admin.site.index_title = "Panel de Control"
+
 urlpatterns = [
     path("", admin.site.urls)
 ]
