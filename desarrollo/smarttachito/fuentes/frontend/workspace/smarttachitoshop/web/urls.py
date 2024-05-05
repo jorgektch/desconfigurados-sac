a8be6e53f2d51e1e03ad220f8c7cf7867e250a80
Jhordan <jhordan.laureano@unmsm.edu.pe>
2024-05-05 15:32:34 -0500

    Crear vista controlador para el catalogo de productos

---
+++ desarrollo/smarttachito/fuentes/frontend/workspace/smarttachitoshop/web/urls.py
@@ -0,0 +1,12 @@
+
+from django.urls import path
+
+from . import views  #from . significa que importame el archivo views
+
+app_name = 'web' #esto para ubicar los enlaces, indicarle en que apliacion esta
+
+#urlpatters es la lista de las rutas
+urlpatterns = [
+    path('', views.index,name='index'), #significa que cuando se entre a la raiz de la pagina, se ejecute la funcion index
+    
+] 
--- desarrollo/smarttachito/fuentes/frontend/workspace/smarttachitoshop/web/views.py
+++ desarrollo/smarttachito/fuentes/frontend/workspace/smarttachitoshop/web/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
+from .models import Categoria,Producto
 # Create your views here.
+"""VISTAS PARA EL CATALOGO DE PRODUCTOS"""
+ 
+
+def index(request): # Vista para la pagina principal
+    listaProductos = Producto.objects.all() # Obtener todos los productos de la base de datos
+    # Crear un diccionario con los productos porque la plantilla espera un diccionario  
+    print(listaProductos)
+    context={
+        'productos':listaProductos #enviando un diccionario con primarea clave productos cuyo valor tiene un listado que vien dela DB
+    }
+    return render(request, 'index.html',context)    # Mostrar para comprobar las plantillas
