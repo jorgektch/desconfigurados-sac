6d0cc6e63374fd407afa137bbf3a4969b2019503
Jhordan <jhordan.laureano@unmsm.edu.pe>
2024-05-04 22:10:08 -0500

    Crear y configurar panel de administracion

--- desarrollo/smarttachito/fuentes/frontend/workspace/smarttachitoshop/web/admin.py
+++ desarrollo/smarttachito/fuentes/frontend/workspace/smarttachitoshop/web/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Categoria, Producto
+
+
+admin.site.register(Categoria)
+#admin.site.register(Producto)
+
+@admin.register(Producto)
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'precio', 'categoria', 'imagen','fecha_registro') #que quiero que se muestre
+    list_editable = ('precio',) #que se puede editar
+
