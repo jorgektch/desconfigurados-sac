d3bf5566dc7259550fdef5cdd75a97ab0f4eda08
Jhordan <jhordan.laureano@unmsm.edu.pe>
2024-05-04 21:08:49 -0500

    Configurar rutas estaticas al proyecto como jpg

--- desarrollo/smarttachito/fuentes/frontend/workspace/smarttachitoshop/smarttachitoshop/settings.py
+++ desarrollo/smarttachito/fuentes/frontend/workspace/smarttachitoshop/smarttachitoshop/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -128,3 +128,9 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#el join unira dos path osea dos rutas
+#ruta donde se guardaran las imagenes
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+#url donde se guardaran las imagenes hasta el directorio dentro del proyecto
+MEDIA_URL = '/media/'
