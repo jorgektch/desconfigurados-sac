5753302af3cd34cb1846f445ec6ac42fe4981143
Jorge Ktch <jorgektch@gmail.com>
2024-05-06 12:45:11 -0500

    Actualizar view

--- desarrollo/smarttachito/fuentes/backend/ventas/views.py
+++ desarrollo/smarttachito/fuentes/backend/ventas/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework import permissions
+from .serializers import *
+from .models import *
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
