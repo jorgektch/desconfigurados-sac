1ee4c572cdc073aaa579d8cd65bf3cf518903e57
Jorge Ktch <jorgektch@gmail.com>
2024-05-06 12:58:42 -0500

    Actualizar views

--- desarrollo/smarttachito/fuentes/backend/reparto/views.py
+++ desarrollo/smarttachito/fuentes/backend/reparto/views.py
@@ -5,3 +5,20 @@ from .serializers import *
 from .models import *
 from rest_framework.permissions import AllowAny
 
+class UbicacionViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows Universities to be viewed or edited.
+    """
+    queryset = Ubicacion.objects.all()
+    serializer_class = UbicacionSerializer
+    permission_classes = [AllowAny]  # Cambiamos IsAuthenticated por AllowAny
+    filterset_fields = ['ciudad'] # Nuevo API filter
+
+class EntregaViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows Universities to be viewed or edited.
+    """
+    queryset = Entrega.objects.all()
+    serializer_class = EntregaSerializer
+    permission_classes = [AllowAny]  # Cambiamos IsAuthenticated por AllowAny
+    filterset_fields = ['ubicacion'] # Nuevo API filter
