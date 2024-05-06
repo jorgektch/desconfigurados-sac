a8eceb76d3925fb52aa890513475cb2abc10b443
Laura <lcmm97.lcmm@gmail.com>
2024-05-05 19:13:59 -0500

    Agregar buscador al catálogo

---
+++ desarrollo/smarttachito/fuentes/frontend/e-smarttachito/src/customer/productos-filtrados/productos-filtrados.component.ts
@@ -0,0 +1,33 @@
+import { Component, Input } from '@angular/core';
+import { Productos } from '../../shared/interfaces/interface';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { HeaderComponent } from "../../shared/layouts/header/header.component";
+import { CarritoServiceService } from '../../shared/services/carrito-service.service';
+
+@Component({
+    selector: 'app-productos-filtrados',
+    standalone: true,
+    templateUrl: './productos-filtrados.component.html',
+    styleUrl: './productos-filtrados.component.css',
+    imports: [
+        CommonModule,
+        RouterModule,
+        HeaderComponent
+    ]
+})
+export class ProductosFiltradosComponent {
+  @Input() producto!: Productos;
+  
+  constructor(private servicioCarrito: CarritoServiceService){}
+
+  //Obtener la imagen compuesta del path y la extension
+  getImagen(img: { path: string; extension: string }): string {
+    return `${img.path}.${img.extension}`;  
+  }
+
+  agregarAlCarrito(producto: Productos) {
+    // código para agregar producto al carrito
+    this.servicioCarrito.agregarProducto(producto);
+  }
+}
--- desarrollo/smarttachito/fuentes/frontend/e-smarttachito/src/customer/vista-catalogo/catalogo/catalogo.component.ts
+++ desarrollo/smarttachito/fuentes/frontend/e-smarttachito/src/customer/vista-catalogo/catalogo/catalogo.component.ts
@@ -1,39 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { NgIf, NgFor, AsyncPipe } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { Productos } from '../../../shared/interfaces/interface';
 import { Observable } from 'rxjs';
 import { ApiPruebaService } from '../../../shared/services/api-prueba.service';
 import { CarritoServiceService } from '../../../shared/services/carrito-service.service';
+import { ProductosFiltradosComponent } from "../../productos-filtrados/productos-filtrados.component";
 
 @Component({
-  selector: 'app-catalogo',
-  standalone: true,
-  imports: [NgIf, NgFor, RouterModule, AsyncPipe],
-  templateUrl: 'catalogo.component.html',
-  styleUrl: 'catalogo.component.css'
+    selector: 'app-catalogo',
+    standalone: true,
+    templateUrl: 'catalogo.component.html',
+    styleUrl: 'catalogo.component.css',
+    imports: [NgIf, NgFor, RouterModule, AsyncPipe, ProductosFiltradosComponent]
 })
 export class CatalogoComponent implements OnInit {
 
+  filtered: Productos [] = [];
+  productos: Productos [] = [];
+  api: ApiPruebaService = inject(ApiPruebaService);
+  constructor(private service: ApiPruebaService, private servicioCarrito: CarritoServiceService ){
+    this.api.getApiProductos().subscribe((_productos: Productos[])=>{
+      this.filtered = _productos;
+      this.productos = _productos;
+    })
+  }
+
   //Define productos$ de tipo Observable<Productos[]> para recuperar la data del producto
   public productos$!: Observable<Productos[]>; 
-
-  //Inyectar el ApiPrueba Service en el constructor
-  constructor(private service: ApiPruebaService, private servicioCarrito: CarritoServiceService ){}
   
   //Siempre al iniciar se va a realizar getApiProductos
   ngOnInit(): void {
     this.productos$ = this.service.getApiProductos();
   }
-
-  //Obtener la imagen compuesta del path y la extension
-  getImagen(img: { path: string; extension: string }): string {
-    return `${img.path}.${img.extension}`;  
-  }
-
-  agregarAlCarrito(producto: Productos) {
-    // código para agregar producto al carrito
-    this.servicioCarrito.agregarProducto(producto);
+  filterResults(text: string){
+    if(!text){
+      this.filtered = this.productos;
+      return;
+    }
+    this.filtered = this.productos.filter(producto =>(
+      producto?.id.toLowerCase().includes(text.toLowerCase()) ||
+      producto?.nombre.toLowerCase().includes(text.toLowerCase())
+    ));
   }
 
 }
