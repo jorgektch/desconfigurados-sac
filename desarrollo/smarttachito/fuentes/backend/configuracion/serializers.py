2fa4003269caf5b7fb68d59ab148a3e3fc83eb71
Jorge Ktch <jorgektch@gmail.com>
2024-05-06 12:21:04 -0500

    Agregar serializers por cada model

---
+++ desarrollo/smarttachito/fuentes/backend/configuracion/serializers.py
@@ -0,0 +1,29 @@
+from django.contrib.auth.models import  Group
+from rest_framework import serializers
+
+from .models import *
+
+class TipoDocumentoSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = TipoDocumento
+        fields = ['nombre', 'descripcion']
+
+class PaisSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Pais
+        fields = ['nombre']
+
+class CiudadSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Ciudad
+        fields = ['pais', 'nombre']
+
+class EstadoEntregaSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = EstadoEntrega
+        fields = ['nombre', 'descripcion']
+
+class EstadoOrdenSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = EstadoOrden
+        fields = ['nombre', 'descripcion']
