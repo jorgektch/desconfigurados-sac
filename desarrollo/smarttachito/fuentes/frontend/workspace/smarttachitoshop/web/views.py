bbdce1cbcc6f7b9c6ea61d503b612c7e81bc87cd
Jhordan <jhordan.laureano@unmsm.edu.pe>
2024-05-06 23:48:37 -0500

    Agregar categorias de la base de datos en catalogo.html

--- desarrollo/smarttachito/fuentes/frontend/workspace/smarttachitoshop/web/views.py
+++ desarrollo/smarttachito/fuentes/frontend/workspace/smarttachitoshop/web/views.py
@@ -6,10 +6,13 @@ from .models import Categoria,Producto
  
 
 def index(request): # Vista para la pagina principal
+    listaCategorias = Categoria.objects.all() # Obtener todas las categorias de la base de datos
     listaProductos = Producto.objects.all() # Obtener todos los productos de la base de datos
+   # listaCategorias = Categoria.objets.all()
     # Crear un diccionario con los productos porque la plantilla espera un diccionario  
-    print(listaProductos)
+   
     context={
-        'productos':listaProductos #enviando un diccionario con primarea clave productos cuyo valor tiene un listado que vien dela DB
+        'productos':listaProductos, #enviando un diccionario con primarea clave productos cuyo valor tiene un listado que vien dela DB
+        'categorias':listaCategorias #enviando un diccionario con primarea clave categorias cuyo valor tiene un listado que vien dela DB
     }
     return render(request, 'index.html',context)    # Mostrar para comprobar las plantillas
