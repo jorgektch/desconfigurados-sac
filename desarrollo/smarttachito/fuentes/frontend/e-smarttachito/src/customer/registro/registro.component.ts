7d6b7c1bd68b5417c5e4b84f4eb11a9b1e64d0d2
Laura <lcmm97.lcmm@gmail.com>
2024-05-09 09:40:37 -0500

    Hacer registro de usuarios

--- desarrollo/smarttachito/fuentes/frontend/e-smarttachito/src/customer/registro/registro.component.ts
+++ desarrollo/smarttachito/fuentes/frontend/e-smarttachito/src/customer/registro/registro.component.ts
@@ -1,47 +1,48 @@
-import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
-import { ApiPruebaService } from '../../shared/services/api-prueba.service';
 import { Usuarios } from '../../shared/interfaces/interface';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { LoginSignupService } from '../../shared/services/login-signup.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-registro',
   standalone: true,
-  imports: [RouterModule],
+  imports: [CommonModule,RouterModule, ReactiveFormsModule, FormsModule],
   templateUrl: './registro.component.html',
   styleUrl: './registro.component.css'
 })
-export class RegistroComponent {
+export class RegistroComponent implements OnInit{
+
   regForm: boolean = false;
-  signUpfrom!: FormGroup;
-  signInfrom!: FormGroup;
+  signUpfrom!:FormGroup;
   signUpsubmitted = false;
   href: string = '';
-  user_data: any;
   user_dto!: Usuarios;
-  user_reg_data:any;
-  signInFormValue:any ={};
+  user_reg_data: any;
+  signInFormValue: any = {};
 
-  constructor(private formBuilder: FormBuilder, private router: Router, loginService: LoginSignupService){ }
-  
-  ngOnInit(): void{
+  constructor(private formBuilder: FormBuilder, private router: Router, private SingUpService: LoginSignupService) { }
+
+  ngOnInit(): void {
     this.href = this.router.url;
-    if(this.href == '/registro'){
+    if (this.href == '/registro') {
       this.regForm = true;
-    }
-    else if(this.href == '/login'){
-      this.regForm = false
+    } else if (this.href == '/login') {
+      this.regForm = false;
     }
 
     this.signUpfrom = this.formBuilder.group({
       nombre: ['', Validators.required],
       apellido: ['', Validators.required],
       usuario: ['', Validators.required],
+      genero: ['', Validators.required],
+      id: ['', Validators.required],
       contrasenia: ['', Validators.required],
-    })
+      role: ['', Validators.required],
+    });
   }
+
   get rf(){
     return this.signUpfrom.controls;
   }
@@ -52,12 +53,18 @@ export class RegistroComponent {
       return;
     }
     this.user_reg_data = this.signUpfrom.value;
-    // this.user_dto = {
-    //   // nombre: this.user_reg_data.nombre,
-    //   apellido: this.user_reg_data.apellido,
-    //   usuario: this.user_reg_data.usuario,
-    //   contrasenia: this.user_reg_data.contrasenia,
-    // }
-    // this.loginService.userRegister()
+    this.user_dto = {
+      nombre: this.user_reg_data.nombre,
+      apellido: this.user_reg_data.apellido,
+      genero: this.user_reg_data.nombre,
+      usuario: this.user_reg_data.nombre,
+      contrasenia: this.user_reg_data.contrasenia,
+      id: this.user_reg_data.id,
+      role: this.user_reg_data.role,
+    }
+    this.SingUpService.registroUsuario(this.user_dto).subscribe(data=>{
+      alert("Usuario registrado con Éxito :D");
+      this.router.navigateByUrl('/login');
+    })
   }
 }
