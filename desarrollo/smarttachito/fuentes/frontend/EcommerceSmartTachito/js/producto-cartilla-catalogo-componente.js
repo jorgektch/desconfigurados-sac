f678f5c11a4bdcebd62d97e5823cc918a57d4c15
anycodef <anycodefast1@gmail.com>
2024-06-17 00:46:51 -0500

    Eliminar archivos innecesarios en el nuevo enfoque de navegación y carga de contenido

--- desarrollo/smarttachito/fuentes/frontend/EcommerceSmartTachito/js/producto-cartilla-catalogo-componente.js
+++
@@ -1,6 +0,0 @@
-import {loadComponent} from './utils/loadComponent.js'
-
-document.addEventListener("DOMContentLoaded", () => {
-    //cargar los componentes
-    loadComponent("cartilla-catalogo-componente", "../html/components/producto-cartilla-catalogo.html");
-});
