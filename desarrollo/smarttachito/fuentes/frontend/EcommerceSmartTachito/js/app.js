1ee91388242034b6c928dd4426df0316f1752c6c
anycodef <anycodefast1@gmail.com>
2024-06-17 00:17:09 -0500

    Añadir un app.js para la carga del contenido inicial y configurar la navegación total de las vistas

---
+++ desarrollo/smarttachito/fuentes/frontend/EcommerceSmartTachito/js/app.js
@@ -0,0 +1,41 @@
+import { loadComponent } from './utils/loadComponents.js';
+
+document.addEventListener("DOMContentLoader", function () {
+	// cargar header y footer
+	loadComponent("header-placeholder", "./html/components/header.html")
+		.then(() => {
+			// Configurar navegación después de cargar el header
+			setupNavigation();
+		});
+
+	loadComponent("footer-placeholder", "./html/components/footer.html");
+
+	// cargar contenido inicial
+	loadMainContent("./html/landing.html");
+});
+
+function setupNavigation() {
+	document.addEventListener('click', function (event) {
+		const target = event.target;
+
+		// verificar si se a clikcleado un elemento a (enlace)
+		if (target.matches('a')) {
+			// Prevenir la navegación predeterminada
+			event.preventDefault();
+
+			// obtener la URL del enlace
+			const url = target.getAttribute('href');
+
+			// cargar el contenido correspondiente en el <main>
+			loadComponent('contenido-principal', url);
+
+			// cambiar la url en la barra de direcciones sin recargar la página
+			history.pushState(null, '', url);
+		}
+	});
+}
+
+window.addEventListener('popstate', function () {
+	// cargar el contenido de la URL actual
+	loadComponent('contenido-principal', location.pathname);
+});
