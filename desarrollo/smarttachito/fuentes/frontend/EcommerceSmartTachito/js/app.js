dc0a52f7a75d7abba733d7d4aa1def0be568b853
anycodef <anycodefast1@gmail.com>
2024-06-17 03:06:04 -0500

    Añadir diccionario de rutas

--- desarrollo/smarttachito/fuentes/frontend/EcommerceSmartTachito/js/app.js
+++ desarrollo/smarttachito/fuentes/frontend/EcommerceSmartTachito/js/app.js
@@ -1,5 +1,16 @@
 import { loadComponent } from './utils/loadComponents.js';
 
+const rutas = {
+    "landing"  : "html/landing.html",
+    "catalogo" : "html/catalogo.html",
+    "novedades": "html/landing.html",
+    "nosotros" : "html/landing.html",
+    "contacto" : "html/landing.html",
+    "inicio-sesion": "html/inicio-sesion.html",
+    "registro": "html/registro.html",
+    "carrito": "html/carrito.html"
+}
+
 document.addEventListener("DOMContentLoaded", function () {
 	// cargar header y footer
 	loadComponent("header-placeholder", "../html/components/header.html")
@@ -11,31 +22,34 @@ document.addEventListener("DOMContentLoaded", function () {
 	loadComponent("footer-placeholder", "../html/components/footer.html");
 
 	// cargar contenido inicial
-	loadComponent('contenido-principal', "../html/landing.html");
+	loadComponent('contenido-principal', rutas['landing']);
 });
 
 function setupNavigation() {
 	document.addEventListener('click', function (event) {
-		const target = event.target;
+		const target = event.target.closest('a');
 
 		// verificar si se a clikcleado un elemento a (enlace)
-		if (target.matches('a')) {
+		if (target) {
 			// Prevenir la navegación predeterminada
 			event.preventDefault();
 
 			// obtener la URL del enlace
-			const url = target.getAttribute('href');
+			const page_name = target.getAttribute('href');
+
+			const absoluteUrl = location.origin + '/' + page_name;
+            console.log(absoluteUrl);
 
 			// cargar el contenido correspondiente en el <main>
-			loadComponent('contenido-principal', url);
-
+			loadComponent('contenido-principal', rutas[page_name]);
 			// cambiar la url en la barra de direcciones sin recargar la página
-			history.pushState(null, '', url);
+			history.pushState(null, '', absoluteUrl);
 		}
 	});
 }
 
+// Evento para manejar cambios en el historial del navegador
 window.addEventListener('popstate', function () {
-	// cargar el contenido de la URL actual
-	loadComponent('contenido-principal', location.pathname);
+    const path = location.pathname.replace(/^\/([^\/]*).*$/, '$1');
+    loadComponent('contenido-principal', rutas[path]);
 });
