ad8397f95bc5262c012b7a05ef48c43933865103
anycodef <anycodefast1@gmail.com>
2024-06-18 02:16:12 -0500

    Modificar forma de cargar contenido en la página

--- desarrollo/smarttachito/fuentes/frontend/EcommerceSmartTachito/js/app.js
+++ desarrollo/smarttachito/fuentes/frontend/EcommerceSmartTachito/js/app.js
@@ -1,55 +1,47 @@
 import { loadComponent } from './utils/loadComponents.js';
 
-const rutas = {
-    "landing"  : "html/landing.html",
-    "catalogo" : "html/catalogo.html",
-    "novedades": "html/landing.html",
-    "nosotros" : "html/landing.html",
-    "contacto" : "html/landing.html",
-    "inicio-sesion": "html/inicio-sesion.html",
-    "registro": "html/registro.html",
-    "carrito": "html/carrito.html"
+const loadContentIfNotLoad = (sectionId, sectionE) => {
+	if (!sectionE.classList.contains('loaded')) {
+		const contentPath = sectionE.dataset.contentPath;
+		loadComponent(sectionId, contentPath);
+		sectionE.classList.add('loaded');
+	}
 }
+
 
 document.addEventListener("DOMContentLoaded", function () {
 	// cargar header y footer
-	loadComponent("header-placeholder", "../html/components/header.html")
-        .then(() => {
-			// Configurar navegación después de cargar el header
-			setupNavigation();
-		});
+	loadComponent("header-placeholder", "./html/components/header.html")
+	loadComponent("footer-placeholder", "./html/components/footer.html");
+	loadComponent("landing-seccion", "./html/landing.html");
 
-	loadComponent("footer-placeholder", "../html/components/footer.html");
-
-	// cargar contenido inicial
-	loadComponent('contenido-principal', rutas['landing']);
-});
-
-function setupNavigation() {
-	document.addEventListener('click', function (event) {
-		const target = event.target.closest('a');
-
-		// verificar si se a clikcleado un elemento a (enlace)
-		if (target) {
-			// Prevenir la navegación predeterminada
+	// Manejador de click para la navegación en el header
+	document.getElementById('header-placeholder').addEventListener('click', (event) => {
+		console.log(event.target.tagName);
+		if (event.target.tagName === 'A' && event.target.dataset.section) {
+			console.log('click');
 			event.preventDefault();
 
-			// obtener la URL del enlace
-			const page_name = target.getAttribute('href');
+			const sectionId = event.target.dataset.section;
+			const sectionElement = document.getElementById(sectionId);
 
-			const absoluteUrl = location.origin + '/' + page_name;
-            console.log(absoluteUrl);
+			loadContentIfNotLoad(sectionId, sectionElement);
 
-			// cargar el contenido correspondiente en el <main>
-			loadComponent('contenido-principal', rutas[page_name]);
-			// cambiar la url en la barra de direcciones sin recargar la página
-			history.pushState(null, '', absoluteUrl);
+			sectionElement.scrollIntoView({ behavior: 'smooth' });
 		}
 	});
-}
 
-// Evento para manejar cambios en el historial del navegador
-window.addEventListener('popstate', function () {
-    const path = location.pathname.replace(/^\/([^\/]*).*$/, '$1');
-    loadComponent('contenido-principal', rutas[path]);
+	// Manejar la carga de contenido dinámico basado en el scroll
+	window.addEventListener('scroll', () => {
+		const sections = document.querySelectorAll('section');
+		sections.forEach(section => {
+			const rect = section.getBoundingClientRect();
+			
+			if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
+				loadContentIfNotLoad(section.id, section);
+			}
+		})
+	});
+	
 });
+
