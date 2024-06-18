c1b512b960394804b65acf89dbc362a25e4bb96d
anycodef <anycodefast1@gmail.com>
2024-06-18 04:40:20 -0500

    Modificar para cargar scripts de componentes y usar async

--- desarrollo/smarttachito/fuentes/frontend/EcommerceSmartTachito/js/app.js
+++ desarrollo/smarttachito/fuentes/frontend/EcommerceSmartTachito/js/app.js
@@ -1,46 +1,61 @@
 import { loadComponent } from './utils/loadComponents.js';
 
-const loadContentIfNotLoad = (sectionId, sectionE) => {
+const loadContentIfNotLoad = async (sectionId) => {
+	const sectionE = document.getElementById(sectionId);
+
 	if (!sectionE.classList.contains('loaded')) {
 		const contentPath = sectionE.dataset.contentPath;
-		loadComponent(sectionId, contentPath);
+		await loadComponent(sectionId, contentPath)
+		
+		await loadAllScriptInSection(sectionE);
+
 		sectionE.classList.add('loaded');
 	}
 }
 
+const loadAllScriptInSection = async (sectionE) => {
+	console.log(sectionE)
+	console.log(sectionE.querySelectorAll('script'));
+	sectionE.querySelectorAll('script').forEach(script => {
+		const newScript = document.createElement('script');
+		newScript.src = script.src;
+		newScript.type = script.type || 'text/javascript';
+		document.body.appendChild(newScript);
+	});
+}
 
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", async function () {
 	// cargar header y footer
-	loadComponent("header-placeholder", "./html/components/header.html")
-	loadComponent("footer-placeholder", "./html/components/footer.html");
-	loadComponent("landing-seccion", "./html/landing.html");
+	await loadComponent("header-placeholder", "./html/components/header.html")
+	await loadComponent("footer-placeholder", "./html/components/footer.html");
+	await loadComponent("landing-seccion", "./html/landing.html");
 
 	// Manejador de click para la navegación en el header
-	document.getElementById('header-placeholder').addEventListener('click', (event) => {
+	document.getElementById('header-placeholder').addEventListener('click', async (event) => {
 		console.log(event.target.tagName);
 		if (event.target.tagName === 'A' && event.target.dataset.section) {
 			console.log('click');
 			event.preventDefault();
 
 			const sectionId = event.target.dataset.section;
-			const sectionElement = document.getElementById(sectionId);
+			const section = document.getElementById(sectionId);
 
-			loadContentIfNotLoad(sectionId, sectionElement);
+			await loadContentIfNotLoad(sectionId);
 
-			sectionElement.scrollIntoView({ behavior: 'smooth' });
+			section.scrollIntoView({ behavior: 'smooth' });
 		}
 	});
 
 	// Manejar la carga de contenido dinámico basado en el scroll
-	window.addEventListener('scroll', () => {
+	window.addEventListener('scroll', async () => {
 		const sections = document.querySelectorAll('section');
-		sections.forEach(section => {
+		for ( const section of sections) {
 			const rect = section.getBoundingClientRect();
 			
 			if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
-				loadContentIfNotLoad(section.id, section);
+				await loadContentIfNotLoad(section.id);
 			}
-		})
+		}
 	});
 	
 });
