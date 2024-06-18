ae7696e68154af9ee54f803d5fd82a9a598d8b30
anycodef <anycodefast1@gmail.com>
2024-06-18 05:20:10 -0500

    Modificar carga inicial de secciones

--- desarrollo/smarttachito/fuentes/frontend/EcommerceSmartTachito/js/app.js
+++ desarrollo/smarttachito/fuentes/frontend/EcommerceSmartTachito/js/app.js
@@ -1,11 +1,12 @@
 import { loadComponent } from './utils/loadComponents.js';
 
-const loadContentIfNotLoad = async (sectionId) => {
-	const sectionE = document.getElementById(sectionId);
+const loadSection = async (section) => {
+	const sectionE = section;
+	const rect = sectionE.getBoundingClientRect();
 
-	if (!sectionE.classList.contains('loaded')) {
+	if (!sectionE.classList.contains('loaded') && rect.top >= 0 && rect.bottom <= window.innerHeight) {
 		const contentPath = sectionE.dataset.contentPath;
-		await loadComponent(sectionId, contentPath)
+		await loadComponent(sectionE.id, contentPath)
 		
 		await loadAllScriptInSection(sectionE);
 
@@ -13,9 +14,14 @@ const loadContentIfNotLoad = async (sectionId) => {
 	}
 }
 
+const loadSections = async () => {
+	const sections = document.querySelectorAll('section');
+	for (const section of sections) {
+		await loadSection(section);
+	}
+}
+
 const loadAllScriptInSection = async (sectionE) => {
-	console.log(sectionE)
-	console.log(sectionE.querySelectorAll('script'));
 	sectionE.querySelectorAll('script').forEach(script => {
 		const newScript = document.createElement('script');
 		newScript.src = script.src;
@@ -28,19 +34,18 @@ document.addEventListener("DOMContentLoaded", async function () {
 	// cargar header y footer
 	await loadComponent("header-placeholder", "./html/components/header.html")
 	await loadComponent("footer-placeholder", "./html/components/footer.html");
-	await loadComponent("landing-seccion", "./html/landing.html");
+	await loadSections();
 
 	// Manejador de click para la navegación en el header
 	document.getElementById('header-placeholder').addEventListener('click', async (event) => {
-		console.log(event.target.tagName);
+		
 		if (event.target.tagName === 'A' && event.target.dataset.section) {
-			console.log('click');
 			event.preventDefault();
 
 			const sectionId = event.target.dataset.section;
 			const section = document.getElementById(sectionId);
 
-			await loadContentIfNotLoad(sectionId);
+			await loadSection(section);
 
 			section.scrollIntoView({ behavior: 'smooth' });
 		}
@@ -48,14 +53,7 @@ document.addEventListener("DOMContentLoaded", async function () {
 
 	// Manejar la carga de contenido dinámico basado en el scroll
 	window.addEventListener('scroll', async () => {
-		const sections = document.querySelectorAll('section');
-		for ( const section of sections) {
-			const rect = section.getBoundingClientRect();
-			
-			if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
-				await loadContentIfNotLoad(section.id);
-			}
-		}
+		await loadSections();
 	});
 	
 });
